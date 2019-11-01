2f76014851323bad2f72cb95fb1a198b88e775fc
ghostian <junxiao.guo@outlook.com>
2019-11-01 14:31:19 -0400

    feat: train & dehazer

--- model_train.py
+++ model_train.py
@@ -14,22 +14,22 @@ import numpy as np
 
 
 def init_parser():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--path_clearimg', type=str, default="data/images/")
-	parser.add_argument('--path_hazyimg', type=str, default="data/data/")
-	parser.add_argument('--lr', type=float, default=0.0001)
-	parser.add_argument('--weight_decay', type=float, default=0.0001)
-	parser.add_argument('--grad_clip_norm', type=float, default=0.1)
-	parser.add_argument('--num_epochs', type=int, default=10)
-	parser.add_argument('--train_batch_size', type=int, default=8)
-	parser.add_argument('--val_batch_size', type=int, default=8)
-	parser.add_argument('--num_workers', type=int, default=4)
-	parser.add_argument('--display_iter', type=int, default=10)
-	parser.add_argument('--snapshot_iter', type=int, default=200)
-	parser.add_argument('--snapshots_folder', type=str, default="snapshots/")
-	parser.add_argument('--sample_output_folder', type=str, default="samples/")
-	config = parser.parse_args()
-	return config
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_clearimg', type=str, default="data/images/")
+    parser.add_argument('--path_hazyimg', type=str, default="data/data/")
+    parser.add_argument('--lr', type=float, default=0.0001)
+    parser.add_argument('--weight_decay', type=float, default=0.0001)
+    parser.add_argument('--grad_clip_norm', type=float, default=0.1)
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--train_batch_size', type=int, default=8)
+    parser.add_argument('--val_batch_size', type=int, default=8)
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--display_iter', type=int, default=10)
+    parser.add_argument('--snapshot_iter', type=int, default=200)
+    parser.add_argument('--snapshots_folder', type=str, default="snapshots/")
+    parser.add_argument('--sample_output_folder', type=str, default="samples/")
+    config = parser.parse_args()
+    return config
 
 
 def weights_init(m):
@@ -40,49 +40,58 @@ def weights_init(m):
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
+
 def train(config):
 
-	dehaze_net = net.AODNet().cuda()
-	dehaze_net.apply(weights_init)
+    dehaze_net = net.AODNet().cuda()
+    dehaze_net.apply(weights_init)
 
-	train_dataset = dataloader.dehazing_loader(config.path_clearimg, config.path_hazyimg)		
-	val_dataset = dataloader.dehazing_loader(config.path_clearimg,config.path_hazyimg, mode="val")		
-	train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.train_batch_size, shuffle=True, num_workers=config.num_workers, pin_memory=True)
-	val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=config.val_batch_size, shuffle=True, num_workers=config.num_workers, pin_memory=True)
-	criterion = nn.MSELoss().cuda()
-	optimizer = torch.optim.Adam(dehaze_net.parameters(), lr=config.lr, weight_decay=config.weight_decay)
-	dehaze_net.train()
-	for epoch in range(config.num_epochs):
-		for iteration, (clear_img, hazy_img) in enumerate(train_loader):
-			clear_img = clear_img.cuda()
-			hazy_img = hazy_img.cuda()
-			clean_image = dehaze_net(hazy_img)
-			loss = criterion(clean_image, clear_img)
-			optimizer.zero_grad()
-			loss.backward()
-			torch.nn.utils.clip_grad_norm(dehaze_net.parameters(),config.grad_clip_norm)
-			optimizer.step()
+    train_dataset = dataloader.dehazing_loader(
+        config.path_clearimg, config.path_hazyimg)
+    val_dataset = dataloader.dehazing_loader(
+        config.path_clearimg, config.path_hazyimg, mode="val")
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=config.train_batch_size, shuffle=True, num_workers=config.num_workers, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=config.val_batch_size, shuffle=True, num_workers=config.num_workers, pin_memory=True)
+    criterion = nn.MSELoss().cuda()
+    optimizer = torch.optim.Adam(dehaze_net.parameters(
+    ), lr=config.lr, weight_decay=config.weight_decay)
+    dehaze_net.train()
+    for epoch in range(config.num_epochs):
+        for iteration, (clear_img, hazy_img) in enumerate(train_loader):
+            clear_img = clear_img.cuda()
+            hazy_img = hazy_img.cuda()
+            clean_image = dehaze_net(hazy_img)
+            loss = criterion(clean_image, clear_img)
+            optimizer.zero_grad()
+            loss.backward()
+            torch.nn.utils.clip_grad_norm(
+                dehaze_net.parameters(), config.grad_clip_norm)
+            optimizer.step()
 
-			if ((iteration+1) % config.display_iter) == 0:
-				print("Loss at iteration", iteration+1, ":", loss.item())
-			if ((iteration+1) % config.snapshot_iter) == 0:
-				torch.save(dehaze_net.state_dict(), config.snapshots_folder + "Epoch" + str(epoch) + '.pt') 		
+            if ((iteration+1) % config.display_iter) == 0:
+                print("Loss at iteration", iteration+1, ":", loss.item())
+            if ((iteration+1) % config.snapshot_iter) == 0:
+                torch.save(dehaze_net.state_dict(),
+                           config.snapshots_folder + "Epoch" + str(epoch) + '.pt')
 
-		# Validation Stage
-		for iter_val, (clear_img, hazy_img) in enumerate(val_loader):
-			clear_img = clear_img.cuda()
-			hazy_img = hazy_img.cuda()
-			clean_image = dehaze_net(hazy_img)
-			torchvision.utils.save_image(torch.cat((hazy_img, clean_image, clear_img),0), config.sample_output_folder+str(iter_val+1)+".jpg")
-		torch.save(dehaze_net.state_dict(), config.snapshots_folder + "dehaze_model.pt") 
-
+        # Validation Stage
+        for iter_val, (clear_img, hazy_img) in enumerate(val_loader):
+            clear_img = clear_img.cuda()
+            hazy_img = hazy_img.cuda()
+            clean_image = dehaze_net(hazy_img)
+            torchvision.utils.save_image(torch.cat(
+                (hazy_img, clean_image, clear_img), 0), config.sample_output_folder+str(iter_val+1)+".jpg")
+        torch.save(dehaze_net.state_dict(),
+                   config.snapshots_folder + "dehaze_model.pt")
 
 
 if __name__ == "__main__":
-	config = init_parser()
-	if not os.path.exists(config.snapshots_folder):
-		os.mkdir(config.snapshots_folder)
-	if not os.path.exists(config.sample_output_folder):
-		os.mkdir(config.sample_output_folder)
+    config = init_parser()
+    if not os.path.exists(config.snapshots_folder):
+        os.mkdir(config.snapshots_folder)
+    if not os.path.exists(config.sample_output_folder):
+        os.mkdir(config.sample_output_folder)
 
-	train(config)
+    train(config)
--- run_dehazer.py
+++ run_dehazer.py
@@ -17,24 +17,25 @@ import net
 
 def dehaze(hazyimg_path):
 
-	data_hazy = Image.open(hazyimg_path)
-	data_hazy = (np.asarray(data_hazy)/255.0)
-	data_hazy = torch.from_numpy(data_hazy).float()
-	data_hazy = data_hazy.permute(2,0,1)
-	data_hazy = data_hazy.cuda().unsqueeze(0)
+    data_hazy = Image.open(hazyimg_path)
+    data_hazy = (np.asarray(data_hazy)/255.0)
+    data_hazy = torch.from_numpy(data_hazy).float()
+    data_hazy = data_hazy.permute(2, 0, 1)
+    data_hazy = data_hazy.cuda().unsqueeze(0)
 
-	dehaze_net = net.dehaze_net().cuda()
-	dehaze_net.load_state_dict(torch.load('snapshots/dehazer.pt'))
+    dehaze_net = net.dehaze_net().cuda()
+    dehaze_net.load_state_dict(torch.load('snapshots/dehazer.pt'))
 
-	clean_image = dehaze_net(data_hazy)
-	torchvision.utils.save_image(torch.cat((data_hazy, clean_image),0), "results/" + hazyimg_path.split("/")[-1])
-	
+    clean_image = dehaze_net(data_hazy)
+    torchvision.utils.save_image(torch.cat(
+        (data_hazy, clean_image), 0), "results/" + hazyimg_path.split("/")[-1])
+
 
 if __name__ == '__main__':
 
-	test_list = glob.glob("test_images/*")
+    test_list = glob.glob("test_images/*")
 
-	for image in test_list:
+    for image in test_list:
 
-		dehaze(image)
-		print(image, "done!")
+        dehaze(image)
+        print(image, "done!")
