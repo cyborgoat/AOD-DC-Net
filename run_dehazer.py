194c3ae03ba88e8611aed54742392ab4b892e46a
Nersuryc <shaqren@gmail.com>
2019-12-01 16:55:28 -0500

    Tuning Hyperparameters

--- run_dehazer.py
+++ run_dehazer.py
@@ -14,17 +14,19 @@ def dehaze(hazyimg_path, clearimg_path):
     data_hazy = Image.open(hazyimg_path)
     data_hazy = (np.asarray(data_hazy) / 255.0)
     data_hazy = torch.from_numpy(data_hazy).float()
+    data_hazy = data_hazy.unsqueeze(2)
     data_hazy = data_hazy.permute(2, 0, 1)
     data_hazy = data_hazy.to(device).unsqueeze(0)
 
     data_clear = Image.open(clearimg_path)
     data_clear = (np.asarray(data_clear) / 255.0)
     data_clear = torch.from_numpy(data_clear).float()
+    data_hazy = data_hazy.unsqueeze(2)
     data_clear = data_clear.permute(2, 0, 1)
     data_clear = data_clear.to(device).unsqueeze(0)
 
     dehaze_net = net.AODNet().to(device)
-    dehaze_net.load_state_dict(torch.load('snapshots/dehaze_model.pt'))
+    dehaze_net.load_state_dict(torch.load('snapshots/dehaze_model_with_dcp.pt'))
 
     clean_image = dehaze_net(data_hazy)
     torchvision.utils.save_image(torch.cat(
